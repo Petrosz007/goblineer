8b689450c44133d166c1dc3ac49962b5cb8c25ae
Petrosz007 <andipeti2000@gmail.com>
2017-07-14 00:19:48 +0200

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,16 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$schema = 'wowah';
+$apiKey = '6szbehcvdvjn8eccd2s496sr92956c87';
+$realmName = 'ragnaros';
+$realmRegion = 'eu';
+
+
+$conn = new mysqli($servername, $username, $password, $schema);
+
+$conn->set_charset('utf8');
+if ($conn->connect_error) {
+   die('Connection failed: ' . $conn->connect_error);
+}
