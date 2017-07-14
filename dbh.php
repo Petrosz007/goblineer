9e40cf6f66ab84b72df2f22cdb9e5ee2eaec7558
Petrosz007 <andipeti2000@gmail.com>
2017-07-14 20:48:15 +0200

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,16 +1,15 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
+$servername = '';
+$username = '';
 $password = '';
-$schema = 'wowah';
-$apiKey = '6szbehcvdvjn8eccd2s496sr92956c87';
-$realmName = 'ragnaros';
-$realmRegion = 'eu';
-
+$schema = '';
+$apiKey = '';
+$realmName = '';
+$realmRegion = '';
 
 $conn = new mysqli($servername, $username, $password, $schema);
-
 $conn->set_charset('utf8');
+
 if ($conn->connect_error) {
    die('Connection failed: ' . $conn->connect_error);
 }
