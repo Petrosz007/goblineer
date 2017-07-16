16466482427ea002995a6f7c49b1ab5db450ae8d
Andi Péter <andipeti2000@gmail.com>
2017-07-16 21:19:14 +0200

    Added automatic updating by cron

---
+++ cron/cron_includes.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/cron_dbh.php";
+require __DIR__ . "/cron_marketvalue.inc.php";
+require __DIR__ . "/cron_bloodprice.inc.php";
+
+
+?>
--- index.php
+++ index.php
@@ -200,28 +200,29 @@ $last_updated = substr($last_updated_unix_row["MAX(realm)"], 0, -3);
    <?php echo "<span id='lastUpdate' style='display: none;'>".$last_updated_unix."</span>"; ?>
    <script>
      var lastUpdate = document.getElementById('lastUpdate').innerHTML;
-     
+
      var x = setInterval(function(){
         var now = new Date().getTime();
 
         var distance = now - lastUpdate;
 
-        var days = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 * 24));
+        var days = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 * 60 *24));
         var hours = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 
 
-            if(days > 1){
-               document.getElementById("updated").innerHTML = "<strong>"+ days + " days " + hours + " hours ago.</strong>";
-            } else if(hours > 1){
-               document.getElementById("updated").innerHTML = "<strong>"+ hours + "h " + minutes + "m ago.</strong>";
-            } else if(minutes > 1) {
-               document.getElementById("updated").innerHTML = "<strong>"+ minutes + "m " + seconds + "s ago.</strong>";
-            } else {
-               document.getElementById("updated").innerHTML = "<strong>"+ seconds + "s.</strong>";
-            }
+        if(days > 1){
+           document.getElementById("updated").innerHTML = "<strong>"+ days + " days " + hours + " hours ago.</strong>";
+        } else if(hours > 1){
+           document.getElementById("updated").innerHTML = "<strong>"+ hours + "h " + minutes + "m ago.</strong>";
+        } else if(minutes > 1) {
+           document.getElementById("updated").innerHTML = "<strong>"+ minutes + "m " + seconds + "s ago.</strong>";
+        } else {
+           document.getElementById("updated").innerHTML = "<strong>"+ seconds + "s.</strong>";
+        }
+
 
      }, 1000);
    </script>
