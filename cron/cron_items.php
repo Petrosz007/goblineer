1c4a969606e8a798f0a1114fb0f33a830ada1196
Petrosz007 <andipeti2000@gmail.com>
2017-07-30 16:32:33 +0200

    Updated item name updated

--- cron/cron_items.php
+++ cron/cron_items.php
@@ -3,7 +3,7 @@
 ini_set('max_execution_time', 300);
 require __DIR__ . "/cron_includes.php";
 
-$sql = "SELECT item FROM auctions GROUP BY item";
+$sql = "SELECT DISTINCT item FROM auctions";
 $result = mysqli_query($conn, $sql);
 
 
@@ -11,41 +11,58 @@ $sql2 = "SELECT * FROM items";
 $result2 = mysqli_query($conn, $sql2);
 $alreadydid = array();
 
+$last_sql = "SELECT MAX(last) AS last FROM item_status";
+$last_result = mysqli_query($conn, $last_sql);
+$last = mysqli_fetch_assoc($last_result)['last'];
+echo "Last: ".$last. PHP_EOL;
 
 while($row = mysqli_fetch_assoc($result2)){
     array_push($alreadydid, $row['item']);
 }
 
 
-
-
 $sql = 'INSERT INTO items (item, name) VALUES ';
 $i = 0;
 
+
+
 while($row = mysqli_fetch_assoc($result)){
     if(!in_array($row['item'], $alreadydid)){
-        $response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/item/'. $row['item'] .'?locale=en_GB&apikey=' . $apiKey);
-        $responseObject = json_decode($response, true)['name'];
-        
-        $name = str_replace('"','""',$responseObject);
-        $sql = $sql . ' ('.$row['item'].', "'.$name.'"),';
-        ++$i;
-        if($i == 50) {
-            $sql = substr($sql, 0, -1);
-            $sql = $sql .";";
-            mysqli_query($conn, $sql);
-            //echo "line 35 ".$sql;
-            exit();
+        $response = @file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/item/'. $row['item'] .'?locale=en_GB&apikey=' . $apiKey);
+        var_dump($response);
+        echo "Before ".$row['item'] ."  auc  ". $i. PHP_EOL;
+
+        if($response != false){
+            echo "After  ".$row['item'] ."  auc  ". $i. PHP_EOL;
+            $response = file_get_contents('https://'. $realmRegion .'.api.battle.net/wow/item/'. $row['item'] .'?locale=en_GB&apikey=' . $apiKey);
+
+            
+            $responseObject = json_decode($response, true)['name'];
+            
+            $name = str_replace('"','""',$responseObject);
+            $sql = $sql . ' ('.$row['item'].', "'.$name.'"),';
+            ++$i;
+            if($i == 15) {
+                $sql = substr($sql, 0, -1);
+                $sql = $sql .";";
+                mysqli_query($conn, $sql);
+                exit();
+
+            }
         }
     }
 }
+
+
+
+
+
 
 
 if($i > 0){
     $sql = substr($sql, 0, -1);
     $sql = $sql .";";
     mysqli_query($conn, $sql);
-    //echo "line 46 " . $sql;
 }
 
 
