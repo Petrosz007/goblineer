dd9db16369f5c90ebb8addeb3aacfdc5210f5230
Petrosz007 <andipeti2000@gmail.com>
2017-11-26 14:32:15 +0100

    Creates mv.json which can be used by other applications

--- cron/cron_mv_all.php
+++ cron/cron_mv_all.php
@@ -19,6 +19,28 @@ while($row = mysqli_fetch_assoc($result)){
     echo "MV for item ".$row['item']." complete, ".$i." completed.". PHP_EOL;
 }
 
+
+
+
+//Creating mv.json, which will be used in the in-game addon
+$selectSql = "SELECT item, marketvalue, quantity FROM marketvalue";
+$selectResult = mysqli_query($conn, $selectSql);
+echo "Query done...". PHP_EOL;
+$allItems = array();
+while($row = mysqli_fetch_assoc($selectResult)){
+    $allItems[] = $row;
+}
+echo "Created Array...". PHP_EOL;
+//write to json file
+$fp = fopen(__DIR__ . '/../json/mv.json', 'w');
+fwrite($fp, json_encode($allItems));
+fclose($fp);
+echo "File created!". PHP_EOL;
+
+
+
+
+
 // Script end
 function rutime($ru, $rus, $index) {
     return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000))
