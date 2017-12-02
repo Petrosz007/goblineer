1015af5c710085f310ca2c122fb3b43237d15bfb
Petrosz007 <andipeti2000@gmail.com>
2017-12-02 16:50:07 +0100

    Creates mv_names.json, which has names and min price aswell

--- cron/cron_mv_all.php
+++ cron/cron_mv_all.php
@@ -22,6 +22,10 @@ while($row = mysqli_fetch_assoc($result)){
 
 
 
+
+
+
+
 //Creating mv.json, which will be used in the in-game addon
 $selectSql = "SELECT item, marketvalue, quantity FROM marketvalue";
 $selectResult = mysqli_query($conn, $selectSql);
@@ -36,6 +40,54 @@ $fp = fopen(__DIR__ . '/../json/mv.json', 'w');
 fwrite($fp, json_encode($allItems));
 fclose($fp);
 echo "File created!". PHP_EOL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Creating mv_names.json, the same as above, but has items names and min buyout as well
+$selectSql =   "SELECT t1.item, t1.name, t1.marketvalue, t1.quantity, t2.MIN
+FROM
+    (SELECT marketvalue.item, name, marketvalue, quantity FROM marketvalue LEFT JOIN items ON marketvalue.item = items.item) as t1
+LEFT JOIN 
+    (SELECT MIN(buyout / quantity)/10000 as MIN, item FROM auctions GROUP BY item) as t2
+ON
+    t1.item = t2.item";
+$selectResult = mysqli_query($conn, $selectSql);
+echo "Query done...". PHP_EOL;
+$allItems = array();
+while($row = mysqli_fetch_assoc($selectResult)){
+$allItems[] = $row;
+}
+echo "Created Array...". PHP_EOL;
+//write to json file
+$fp = fopen(__DIR__ . '/../json/mv_names.json', 'w');
+fwrite($fp, json_encode($allItems));
+fclose($fp);
+echo "File created!". PHP_EOL;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
