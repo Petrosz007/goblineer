ef32517c8a160939072a7f8b2d561963b3172a6d
Petrosz007 <andipeti2000@gmail.com>
2017-12-20 20:34:18 +0100

    Updated highchart scripts for faster charts

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -31,8 +31,9 @@
       <script defer src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
       <script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-      <script defer src="https://code.highcharts.com/highcharts.js"></script>
+  
+      <script defer src="https://code.highcharts.com/stock/highstock.js"></script>
+      <script defer src="http://code.highcharts.com/modules/boost.js"></script>
 
       <script async>
          //Google Analytics
