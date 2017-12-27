d1e839720ffc87c9f9a3c5bad821868a9d447495
Petrosz007 <andipeti2000@gmail.com>
2017-12-27 18:35:28 +0100

    Search typing js is now all in one file

--- js/typeahead.js
+++ js/typeahead.js
@@ -16,10 +16,13 @@ function findMatches(wordToMatch, items){
 function displayMatches() {
     if(this.value.length > 4){
         const matchArray = findMatches(this.value, items);
-        const html = matchArray.slice(0, 30).map(item => {
+        const html = matchArray.slice(0, 7).map(item => {
             const regex = new RegExp(this.value, 'gi');
-            return `
+            /*return `
             <option value="${item.name}">
+            `;*/
+            return `
+            <a href="/item/${item.name}" class="list-group-item">${item.name}</a>
             `;
         }).join('');
         suggestions.innerHTML = html;
@@ -31,3 +34,22 @@ const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+
+
+jQuery(document).ready(() => {
+    // Show hide popover
+    $("#item").on("input", () => {
+        $("#search-dropdown").css('display', 'block');
+    });
+
+    $("#searchSubmit").on("click", () => {
+      let val = $("#item").val()
+      $(location).attr('href', '/item/'+ val);
+    });
+});
+$(document).on("click", (event) => {
+    var $trigger = $("#nav-input-group");
+    if($trigger !== event.target && !$trigger.has(event.target).length){
+        $("#search-dropdown").css('display', 'none');
+    }            
+});
