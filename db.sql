e1b69fc7c2ab8eae6934b798063e7e1c9658902d
Petrosz007 <andipeti2000@gmail.com>
2017-12-27 21:19:05 +0100

    Made the sql file up-to-date

--- db.sql
+++ db.sql
@@ -1,42 +1,191 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auctions`
+--
+
+DROP TABLE IF EXISTS `auctions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auctions` (
-  `auc` int(15) NOT NULL PRIMARY KEY,
+  `auc` int(15) NOT NULL,
   `item` int(15) DEFAULT NULL,
-  `owner` varchar(22) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `owner` varchar(22) CHARACTER SET utf8 COLLATE utf8_hungarian_ci DEFAULT NULL,
   `buyout` int(15) DEFAULT NULL,
-  `quantity` int(15) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `quantity` int(15) DEFAULT NULL,
+  KEY `item` (`item`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `status` (
-  `realm` varchar(21) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+--
+-- Table structure for table `blood`
+--
 
+DROP TABLE IF EXISTS `blood`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `blood` (
-  `id` int(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `item` int(20) NOT NULL,
   `marketvalue` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
   `quantity` int(50) NOT NULL,
-  `bloodprice` varchar(50) COLLATE utf8_hungarian_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `bloodprice` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`),
+  KEY `marketvalue` (`marketvalue`),
+  KEY `bloodprice` (`bloodprice`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `realms` (
-  `id` int(11) NOT NULL,
-  `region` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `realm` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `slug` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `locale` varchar(50) COLLATE utf8_hungarian_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+--
+-- Table structure for table `historical`
+--
 
-CREATE TABLE `marketvalue` (
-  `id` int(12) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `item` int(11) NOT NULL,
-  `marketvalue` varchar(255) COLLATE utf8_hungarian_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
+DROP TABLE IF EXISTS `historical`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `historical` (
-  `id` int(12) NOT NULL,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `item` int(12) NOT NULL,
   `marketvalue` varchar(255) NOT NULL,
   `quantity` int(255) NOT NULL,
-  `date` int(255) NOT NULL
-) ENGINE=MyISAM
+  `date` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`)
+) ENGINE=MyISAM AUTO_INCREMENT=57233951 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historical_2017_09_05`
+--
+
+DROP TABLE IF EXISTS `historical_2017_09_05`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historical_2017_09_05` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `item` int(12) NOT NULL,
+  `marketvalue` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `date` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`)
+) ENGINE=MyISAM AUTO_INCREMENT=3938608 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historical_2017_09_16`
+--
+
+DROP TABLE IF EXISTS `historical_2017_09_16`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historical_2017_09_16` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `item` int(12) NOT NULL,
+  `marketvalue` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `date` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5309454 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historical_2017_09_18`
+--
+
+DROP TABLE IF EXISTS `historical_2017_09_18`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historical_2017_09_18` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `item` int(12) NOT NULL,
+  `marketvalue` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `date` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6443203 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `item` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=17002 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `marketvalue`
+--
+
+DROP TABLE IF EXISTS `marketvalue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marketvalue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `marketvalue` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `quantity` int(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`)
+) ENGINE=MyISAM AUTO_INCREMENT=10802 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `realms`
+--
+
+DROP TABLE IF EXISTS `realms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `realms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `region` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `realm` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `slug` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `locale` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1313 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `status` (
+  `realm` varchar(21) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7925 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
