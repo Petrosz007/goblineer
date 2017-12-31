d0876b499cc7839b6ff8ef1be6e0f1ce4704b5af
Petrosz007 <andipeti2000@gmail.com>
2017-12-31 18:20:18 +0100

    item_array now delivers more data

--- inc/item.inc.php
+++ inc/item.inc.php
@@ -1,5 +1,6 @@
 <?php
-include 'dbh.php';
+$path = $_SERVER['DOCUMENT_ROOT'];
+include_once($path . "/includes.php");
 
 function item($id, $conn){
    $sql_herb = "SELECT MIN(buyout / quantity)/10000 as MIN FROM auctions where item=$id";
@@ -39,16 +40,60 @@ function item_array($ids, $conn)
         $result_item = mysqli_query($conn, $sql_item);
         if (mysqli_num_rows($result_item) > 0) {
             while($row = mysqli_fetch_assoc($result_item)) {
+                $items[$name]["id"] = $id;
                 $items[$name]["min"] =  $row["MIN"];
                 $items[$name]["quantity"] =  $row["quantity"];
+                $items[$name]["marketvalue"] = marketValue($id, $conn);
             }
         } else {
+            $items[$name]["id"] = null;
             $items[$name]["min"] =  null;
             $items[$name]["quantity"] =  null;
+            $items[$name]["marketvalue"] = null;
         }
     }
 
     return $items;
+}
+
+function tableRow($item)
+{
+    echo("
+    <tr>
+      <td><a href='item/".$item["id"]."' class='q3 links' rel='item=".$item["id"]."'></td>
+      <td align='right'>".number_format($item["min"],2)."<span class='gold-g'>g</span></td>
+      <td align='right'>".number_format($item["marketvalue"], 2)."<span class='gold-g'>g</span></td>
+      <td align='right'>".$item["quantity"]."</td>
+    </tr>
+    ");
+}
+
+
+function table($items, $caption)
+{
+    echo('
+    <div class="table-responsive">
+    <table class="table table-striped table-hover table-mats align-center">
+        <caption class="text-center">'.$caption.'</caption>
+        <thead>
+        <th class="tg-9nbt">Item name:</th>
+        <th class="tg-9right">Low buy:</th>
+        <th class="tg-9right">Market Value:</th>
+        <th class="tg-9right">Available:</th>
+        </thead>
+
+        <tbody>');
+
+        foreach($items as $item)
+        {
+            tableRow($item);
+        }
+        
+    echo('
+        </tbody>
+    </table>
+    </div>
+    ');
 }
 
 
