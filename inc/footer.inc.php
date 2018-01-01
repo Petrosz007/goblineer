b8058062dfa53ad36d89d35a2f06932449ead108
Petrosz007 <andipeti2000@gmail.com>
2018-01-01 17:13:51 +0100

    footer should be fixed at the bottom

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,13 +1,14 @@
-            <footer class="text-center col-md-12">
-               <p>Copyright Goblineer © 2017. All Rights Reserved.<br>
-               World of Warcraft © is a registered trademark of Blizzard Entertainment, Inc.<br>
+            
+            
+        </div><!-- Main center column -->
+        <div class="col-md-3 col-sm-1 col-xs-0">
+        </div>
+        <footer class="text-center col-xs-12" id="footer">
+               <p>Copyright Goblineer © <?php echo date("Y"); ?>. All Rights Reserved.
+               World of Warcraft © is a registered trademark of Blizzard Entertainment, Inc.
                Goblineer is not associated with or endorsed by Blizzard Entertainment, Inc.</p>
             </footer>
-            
-            </div><!-- Main center column -->
-         <div class="col-md-3 col-sm-1 col-xs-0">
-         </div>
-         <div id="offline-notification" style="display: none;">
+        <div id="offline-notification" style="display: none;">
                 <div style="padding: 5px;">
                     <div id="inner-message" class="alert alert-danger" role="alert">
                         
@@ -15,12 +16,13 @@
                     </div>
                 </div>
             </div>
-         </div>
+        </div>
       
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
       <script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       <script defer src="https://code.highcharts.com/highcharts.js"></script>
+      <script defer type="text/javascript" src="/js/last_updated.js"></script>
       <script async type="text/javascript" src="/js/typeahead.js"></script>
       <script defer type="text/javascript" src="/js/chart.js"></script>
       <script defer type="text/javascript" src="/js/offline.js"></script>
