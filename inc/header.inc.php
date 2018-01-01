06a60f677b715f64dab5a57ea3bc96df61329849
Petrosz007 <andipeti2000@gmail.com>
2018-01-01 17:41:07 +0100

    Added option for a dark theme (is commented out)

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -29,6 +29,7 @@
       <script>var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true }</script>
 
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <!--DARK THEME <link href="https://bootswatch.com/3/cyborg/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">-->
       
 
       <script async>
