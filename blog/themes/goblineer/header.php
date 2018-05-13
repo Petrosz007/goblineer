a4f01f0ddc7356cfcaf4c25186968b1269fa6252
Petrosz007 <andipeti2000@gmail.com>
2018-05-13 17:16:06 +0200

    Removed blog header title

--- blog/themes/goblineer/header.php
+++ blog/themes/goblineer/header.php
@@ -9,12 +9,12 @@
       <?php include_once($_SERVER['DOCUMENT_ROOT'] . '/inc/header_parts/navbar.php'); ?>
       <div class="container-fluid" id="bootstrap-overrides">
 
-        <div class="blog-header">
+        <!-- <div class="blog-header">
             <div class="blog-header-content">
-              <p class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></p>
-              <!-- <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p> -->
-            </div>
-        </div>
+              <p class="blog-title"><a href="<?php //echo get_bloginfo( 'wpurl' );?>"><?php //echo get_bloginfo( 'name' ); ?></a></p>
+              <!-- <p class="lead blog-description"><?php //echo get_bloginfo( 'description' ); ?></p> -->
+            <!-- </div>
+        </div> -->
 
         <div class="row">
           <div class="col-md-3 col-sm-1 col-xs-0">
