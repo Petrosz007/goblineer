2d8cfab66e84a2e958c8ff48c971a39baa983137
Petrosz007 <andipeti2000@gmail.com>
2018-05-15 16:31:02 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $GLOBALS['conn'] = $conn;
 //blood of sargeras price
 $bloodPrice = bloodPrice($conn);
 
+//Getting the time of the last update
 $last_updated_unix_row = mysqli_fetch_assoc(mysqli_query($conn, "SELECT MAX(realm) FROM status"));
 $last_updated_unix = $last_updated_unix_row["MAX(realm)"];
 $last_updated = substr($last_updated_unix_row["MAX(realm)"], 0, -3);
@@ -34,59 +35,32 @@ $last_updated = substr($last_updated_unix_row["MAX(realm)"], 0, -3);
 
 	<hr>
 	<div class="col-xs-12 text-center">
-		<!-- <div class="list-group col-md-8 col-xs-12"> -->
 
-			<p>Goblineer is a website where you can find out plenty of information about the World of Warcraft auction house, like Market Value, Historical price, and crafting profits!</p>
-		
-	  		<hr>
+		<p>Goblineer is a website where you can find out plenty of information about the World of Warcraft auction house, like Market Value, Historical price, and crafting profits!</p>
+	
+		<hr>
 
-			<p class="text-left">
-				<?php echo "<span id='lastUpdate' style='display: none;'>".$last_updated_unix."</span>"; ?>
+		<p class="text-left">
+			<?php echo "<span id='lastUpdate' style='display: none;'>".$last_updated_unix."</span>"; ?>
+			Last Updated: <span id='updated'></span>
+
+			<a href="#update-collapse" data-toggle="collapse" >?</a><br>
+			<div id="update-collapse" class="collapse">
 				The Blizzard API updates around every 30-40 minutes.<br>
-				Last Updated: <span id='updated'></span><br>
 				Check the API status here: <a href="https://does.theapi.work/">https://does.theapi.work/</a>
-			</p>
-
-			<hr>
-
-			<p class="text-left">
-				<a href='//wowhead.com/item=124124' class='q3 iconmedium1 links' rel='item=124124' class="text-center"></a>
-				:
-				<?php echo $bloodPrice; ?><span class='gold-g'>g </span>
-				<a href='/blood' class="btn btn-default links">See Blood of Sargeras Price in-depth</a>
-			</p>
-
-
-		<!-- </div> -->
-		<!-- <div class="list-group col-md-4 col-xs-12">
-			<h3 class="text-left">Categories</h3>
-			
-			<div class="list-group">
-				<a href="/categories/alchemy" class="list-group-item">Alchemy</a>
-				<a href="/categories/enchanting" class="list-group-item">Enchanting</a>
-				<a href="/categories/inscription" class="list-group-item">Inscription</a>
 			</div>
-			  
-			<div class="list-group">
-				<a href="/categories/herbalism" class="list-group-item">Herbalism</a>
-				<a href="/categories/mining" class="list-group-item">Mining</a>
-				<a href="/categories/skinning" class="list-group-item">Skinning</a>
-			</div>
+		</p>
 
-			<div class="list-group">
-				<a href="/categories/blacksmithing" class="list-group-item">Blacksmithing</a>
-				<a href="/categories/leatherworking" class="list-group-item">Leatherworking</a>
-				<a href="/categories/tailoring" class="list-group-item">Tailoring</a>
-			</div>
 
-			<div class="list-group">
-				<a href="/categories/jewelcrafting" class="list-group-item">Jewelcrafting</a>
-				<a href="/categories/engineering" class="list-group-item">Engineering</a>
-				<a href="/categories/cooking" class="list-group-item">Cooking</a>
-			</div>
-			
-		</div> -->
-		<!--<div class="list-group col-md-4 col-xs-1"></div>-->
+		<hr>
+
+		<h2 class="text-left">
+			<a href='//wowhead.com/item=124124' class='q3 iconmedium1 links' rel='item=124124' class="text-center"></a>
+			:
+			<?php echo $bloodPrice; ?><span class='gold-g'>g </span>
+			<a href='/blood' class="btn btn-default links">See Blood of Sargeras Price in-depth</a>
+		</h2>
+
 	</div>
 	
 
