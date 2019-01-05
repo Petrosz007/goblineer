7a0211483bf39317e84b405bfbb243a6d3083071
Petrosz <andipeti2000@gmail.com>
2019-01-05 11:05:36 +0100

    Updated the addons page

--- addon.php
+++ addon.php
@@ -16,7 +16,7 @@
                 <li>Tooltip for every item currently on the AH!</li>
                 <li>Super fast</li>
             </ul>
-            <p>You may want to download the Goblineer App to keep the addon's data up-to-date.</p>
+            <p>You need to download the Goblineer Updater to keep the addon's data up-to-date.</p>
             <a target="_blank" href="https://www.curseforge.com/wow/addons/goblineer">Download the Goblineer Addon now from CurseForge!</a>
         </div>
     </div>
@@ -24,18 +24,19 @@
 
 <div class="well">
     <div class="row">
-    <div class="col-md-4">    
-        <img src="/img/addon/app.png" class="img-responsive" alt="">
+    <div class="col-md-6">    
+        <img src="/img/addon/updater.png" class="img-responsive" alt="">
     </div>
-    <div class="col-md-8">
-        <h1>Goblineer App</h1>
-        <p>The Goblineer App can automatically update the Auction House data for you!</p>
+    <div class="col-md-6">
+        <h1>Goblineer Updater</h1>
+        <p>The Goblineer Updater updates the Goblineer Addon's data. It's main feature is that it use any realm's data, not just EU-Ragnaros! It can update in seconds.</p>
         <h5>Features:</h5>
         <ul>
-            <li>Automatic updating</li>
-            <li>Running on Windows startup</li>
+            <li>Update any ralm's data</li>
+            <li>Claculates the marketvalues in seconds</li>
+            <li>Works on Windows and macOS</li>
         </ul>
-        <a target="_blank" href="https://github.com/Petrosz007/goblineer-app/releases">Download the latest version of the Goblineer App from Github!</a>
+        <a target="_blank" href="https://github.com/Petrosz007/goblineer-updater/releases">Download the latest version of the Goblineer Updater from Github!</a>
     </div>
     </div>
 </div>
